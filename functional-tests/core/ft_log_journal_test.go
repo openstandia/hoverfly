46961069318c2cc53a0c988acae63e2419afd0b5
Tatsu Ri <li-da@rgsis.com>
2020-05-29 20:56:21 +0900

    fix test cases

--- functional-tests/core/ft_log_journal_test.go
+++ functional-tests/core/ft_log_journal_test.go
@@ -25,17 +25,17 @@ var _ = Describe("When running Hoverfly as a webserver", func() {
 		hoverfly.Stop()
 	})
 
-	Context("and its in simulate mode with flag -journal-file=journal.log", func() {
+	Context("and its in simulate mode with flag -log-level=debug -logs-output=file", func() {
 
 		BeforeEach(func() {
-			hoverfly.Start("-webserver", "-journal-file=journal.log")
+			hoverfly.Start("-webserver", "-log-level=debug", "-logs-output=file")
 			hoverfly.SetMode("simulate")
 			hoverfly.ImportSimulation(testdata.JsonGetAndPost)
 		})
 
 		Context("I can request an endpoint", func() {
 			Context("using GET", func() {
-				It("and it should write journal message (HTTP request/response) into file journal.log", func() {
+				It("and it should write journal message (HTTP request/response) into file hoverfly.log", func() {
 					request := sling.New().Get("http://localhost:" + hoverfly.GetProxyPort() + "/path1")
 					response := functional_tests.DoRequest(request)
 
@@ -44,18 +44,19 @@ var _ = Describe("When running Hoverfly as a webserver", func() {
 
 					Expect(string(responseBody)).To(Equal("body1"))
 
-					journal, err := hoverfly.GetLogFile("journal.log")
+					journal, err := hoverfly.GetLogFile("hoverfly.log")
 					Expect(err).To(BeNil())
-					Expect(journal).To(ContainSubstring(`"path":"/path1"`))
-					Expect(journal).To(ContainSubstring(`"scheme":"http"`))
-					Expect(journal).To(ContainSubstring(`"method":"GET"`))
-					Expect(journal).To(ContainSubstring(`"body":"` + string(responseBody) + `"`))
-					Expect(journal).To(ContainSubstring(`"status":201`))
+					Expect(journal).To(ContainSubstring(`journal entry`))
+					Expect(journal).To(ContainSubstring(`\"path\":\"/path1\"`))
+					Expect(journal).To(ContainSubstring(`\"scheme\":\"http\"`))
+					Expect(journal).To(ContainSubstring(`\"method\":\"GET\"`))
+					Expect(journal).To(ContainSubstring(`\"body\":\"` + string(responseBody) + `\"`))
+					Expect(journal).To(ContainSubstring(`\"status\":201`))
 				})
 			})
 
 			Context("using POST", func() {
-				It("and it should write journal message (HTTP request/response) into file journal.log", func() {
+				It("and it should write journal message (HTTP request/response) into file hoverfly.log", func() {
 					request := sling.New().Post("http://localhost:" + hoverfly.GetProxyPort() + "/path2/resource")
 
 					response := functional_tests.DoRequest(request)
@@ -65,13 +66,14 @@ var _ = Describe("When running Hoverfly as a webserver", func() {
 
 					Expect(string(responseBody)).To(Equal("POST body response"))
 
-					journal, err := hoverfly.GetLogFile("journal.log")
+					journal, err := hoverfly.GetLogFile("hoverfly.log")
 					Expect(err).To(BeNil())
-					Expect(journal).To(ContainSubstring(`"path":"/path2/resource"`))
-					Expect(journal).To(ContainSubstring(`"scheme":"http"`))
-					Expect(journal).To(ContainSubstring(`"method":"POST"`))
-					Expect(journal).To(ContainSubstring(`"body":"` + string(responseBody) + `"`))
-					Expect(journal).To(ContainSubstring(`"status":200`))
+					Expect(journal).To(ContainSubstring(`journal entry`))
+					Expect(journal).To(ContainSubstring(`\"path\":\"/path2/resource\"`))
+					Expect(journal).To(ContainSubstring(`\"scheme\":\"http\"`))
+					Expect(journal).To(ContainSubstring(`\"method\":\"POST\"`))
+					Expect(journal).To(ContainSubstring(`\"body\":\"` + string(responseBody) + `\"`))
+					Expect(journal).To(ContainSubstring(`\"status\":200`))
 				})
 			})
 		})
